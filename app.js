d88d61647e683c85b8ff821cb36e7933f551c104
anasAchaqour <98333509+anasAchaqour@users.noreply.github.com>
2024-02-25 17:12:23 +0100

    ...

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const sqlite3 = require('sqlite3');
 const session = require('express-session');
 const ejs = require('ejs');
 const app = express();
-const port = 3000;
+const port = 3030;
 // Middleware to parse request body
 app.use(express.urlencoded({ extended: true }));
 // Set EJS as the view engine
